13f87aea148fb683c7797b092731336a79ce9766
Satoru Hayasaka <hayasaka@utexas.edu>
2019-02-07 14:28:06 -0600

    Digit PCA cluster analysis, completed

---
+++ clusterExamples/DigitPCAClusterCompleted.py
@@ -0,0 +1,45 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import datasets
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
+
+
+# loading the digits data
+digits = datasets.load_digits()
+digitsX = digits.data    # the data, 1797 x 64 array
+digitsImages = digits.images  # image data, 1797 x 8 x 8
+digitsTargets = digits.target # target information
+digitsFeatureNames = digits.target_names  # digits
+
+# PCA with all possible components
+digitsPCA = PCA(n_components=64)
+digitsPCs = digitsPCA.fit_transform(digitsX)
+
+# plotting the explained variance ratio (a.k.a., Scree plot)
+plt.plot(np.arange(1,65),digitsPCA.explained_variance_ratio_)
+plt.xlabel('Component number')
+plt.ylabel('Proportion of explained variance')
+plt.show()
+
+#### Exercise Solutions Here ####
+# I will go with 12 PCs
+PC = digitsPCs[:,:12]
+
+# K-means clustering with 10 clusters
+km = KMeans(n_clusters=10)  
+km.fit(PC)  # fitting the principal components
+y_clus = km.labels_   # clustering info resulting from K-means
+
+# plotting the PCs (with clusters resulting from K-means)
+plt.scatter(digitsPCs[:,0], digitsPCs[:,1], c=y_clus, marker='+')
+plt.xlabel('PC1')
+plt.ylabel('PC2')
+plt.show()
+
+
+# plotting the PCs
+plt.scatter(digitsPCs[:,0], digitsPCs[:,1], c=digitsTargets, marker='+')
+plt.xlabel('PC1')
+plt.ylabel('PC2')
+plt.show()
