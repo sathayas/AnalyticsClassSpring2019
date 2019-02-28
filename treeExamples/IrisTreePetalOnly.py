8c0c32b7ef0318b9aefdba460dae446c97c489c4
Satoru Hayasaka <hayasaka@utexas.edu>
2019-02-28 10:57:34 -0600

    Imports graphviz library now

--- treeExamples/IrisTreePetalOnly.py
+++ treeExamples/IrisTreePetalOnly.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import graphviz
 from sklearn.datasets import load_iris
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
