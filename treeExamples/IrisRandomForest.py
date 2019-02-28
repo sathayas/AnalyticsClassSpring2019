408feee9051702362e6afbadf76d3676b543dbd2
Satoru Hayasaka <hayasaka@utexas.edu>
2019-02-28 11:13:29 -0600

    visualizing the decision tree

--- treeExamples/IrisRandomForest.py
+++ treeExamples/IrisRandomForest.py
@@ -14,7 +14,7 @@ target_names = iris.target_names
 
 # spliting the data into training and testing data sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4,
-                                                    random_state=2018)
+                                                    random_state=0)
 
 
 # random forest classifier, training & testing
@@ -68,3 +68,15 @@ y_pred = dt.predict(X_test)
 print(confusion_matrix(y_test,y_pred))
 print(classification_report(y_test, y_pred,
                             target_names=target_names))
+
+
+# visualizing a couple of decision trees
+rfsample = rf.estimators_[5]
+dot_data = export_graphviz(rfsample, feature_names=feature_names,
+                           class_names=target_names, 
+                           filled=True, rounded=True,  
+                           special_characters=True, 
+                           out_file=None)
+graph = graphviz.Source(dot_data)
+### Only works on Jupyter notebook. Otherwise I have to create a separate file
+graph
