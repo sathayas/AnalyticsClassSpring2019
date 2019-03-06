4960d7fc20007512fb3f8e989e5b0de0d5a13cdc
Satoru Hayasaka <hayasaka@utexas.edu>
2019-03-06 14:40:29 -0600

    Cryotherapy 5-fold cross validation

---
+++ cvExamples/CryoCV.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import cross_val_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import make_pipeline
+
+
+# loading the data
+CryoData = pd.read_csv('Cryotherapy.csv')
+
+# Creating the data set
+X = np.array(CryoData.loc[:,['Age','Time']])
+y = np.array(CryoData.Success)
+targetNames = ['Failure', 'Success']
+
+
+# A pipeline of stadardization and kNN classifier
+kNN = make_pipeline(StandardScaler(), 
+                    KNeighborsClassifier(15, weights='uniform'))
+
+# 5-fold cross validation
+scores = cross_val_score(kNN, X, y, cv=5)
+print(scores)
