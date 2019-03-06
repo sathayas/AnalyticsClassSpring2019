a80afaeec957e4813aa74a4b0056bbec881e9c13
Satoru Hayasaka <hayasaka@utexas.edu>
2019-03-06 14:35:53 -0600

    Iris data cross validation example

---
+++ cvExamples/IrisCV.py
@@ -0,0 +1,20 @@
+import numpy as np
+from sklearn import datasets
+from sklearn.model_selection import cross_val_score
+from sklearn.neighbors import KNeighborsClassifier
+
+
+# Loading the iris data
+iris = datasets.load_iris()
+X = iris.data[:,[0,3]]  # sepal length and petal width only
+y = iris.target
+
+
+# defining the nearest neighbor classifier
+kNN = KNeighborsClassifier(5, weights='uniform')
+
+# 5-fold cross validation
+scores = cross_val_score(kNN, X, y, cv=5)
+print(scores)
+print(scores.mean())
+
