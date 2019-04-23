3d0cce6885b9c976129bfa140678251c00ff4b51
Satoru Hayasaka <hayasaka@utexas.edu>
2019-04-23 12:07:06 -0500

    Simple word cloud example

---
+++ wcloudExamples/SimpleExample.py
@@ -0,0 +1,31 @@
+import nltk
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+
+# loading Emma by Jane Austen
+from nltk.corpus import gutenberg
+text = gutenberg.raw('austen-emma.txt')
+
+# Generate a word cloud image
+wordcloud = WordCloud(max_font_size=72).generate(text)
+
+# Display the generated image:
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")
+plt.show()
+
+
+
+# loading course syllabus
+f = open('Syllabus_UTFormat_v5.txt','r', encoding = "ISO-8859-1")
+text = f.read()
+f.close()
+
+# Generate a word cloud image
+wordcloud = WordCloud(max_font_size=72, background_color='white').generate(text)
+
+# Display the generated image:
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")
+plt.show()
+
